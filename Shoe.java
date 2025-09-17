05db547792edbe63443a5b33487ba393076ba216
Gagan Shrestha <gaganshresthacr7@gmail.com>
2025-09-17 23:46:26 +1000

    Create Shoe.java

---
+++ Shoe.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+public class Shoe extends Armour {
+
+  /* Class Variables */
+public class someClass implements Serializable{
+  private static final long serialVersion = 1L;
+} 
+
+  /* Constructors */
+  public Shoe(String name, int durability, int defenseRating) {
+    super(name, durability, defenseRating);
+  }
+}
